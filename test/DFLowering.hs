0039a0f82218d7d8cc3cf9bae0312863ee9f57c5
Justus Adam <dev@justus.science>
2017-09-02 18:33:23 +0200

    Somewhat improved error messages for graph comparison

--- test/DFLowering.hs
+++ test/DFLowering.hs
@@ -191,51 +191,81 @@ isomorphicMapping g1 g2 = either (const Nothing) Just $ matchGraph g1 g2
 
 type IsoMap = IntMap.IntMap Int
 
-plusEither :: Monoid a => Either a b -> Either a b -> Either a b
-plusEither (Left a) (Left a2) = Left $ a `mappend` a2
+maxOn :: Ord b => (a -> b) -> a -> a -> a
+maxOn f a0 a1 | ((>=) `on` f) a0 a1 = a0
+              | otherwise = a1
+
+type IsoFailData = (IsoMap, Maybe (Int, Int))
+
+plusEither :: Either IsoFailData b -> Either IsoFailData b -> Either IsoFailData b
+plusEither (Left a) (Left a2) = Left $ maxOn (IntMap.size . fst) a a2
 plusEither r@(Right _) _ = r
 plusEither _ b = b
 
-emptyEither :: Monoid a => Either a b
-emptyEither = Left mempty
+emptyEither :: Either IsoFailData b
+emptyEither = Left (mempty, Nothing)
 
-sumEither :: (Monoid a, Foldable f) => f (Either a b) -> Either a b
+sumEither :: Foldable f => f (Either IsoFailData b) -> Either IsoFailData b
 sumEither = foldl' plusEither emptyEither
 
-matchGraph :: (Eq a, Ord b) => Gr a b -> Gr a b -> Either [IsoMap] IsoMap
-matchGraph gr1 gr2 = go (nodes gr1) [] [] mempty
+mapsEither :: Foldable f => (a -> Either IsoFailData b) -> f a -> Either IsoFailData b
+mapsEither f = foldl' (\a b -> plusEither a (f b)) emptyEither
+
+matchGraph :: (Eq a, Ord b) => Gr a b -> Gr a b -> Either IsoFailData IsoMap
+matchGraph gr1 gr2 | (x:xs) <- nodes gr1 = (\k -> go xs [] [] mempty k x) `mapsEither` nodes gr2
   where
-    go :: [Int] -> [Int] -> [Int] -> IsoMap -> Either [IsoMap] IsoMap
-    go rest !gr1Selected !gr2Selected !mapping | gr1Subgr == rename mapping (subgraph gr2Selected gr2) = descend rest
+    go :: [Int] -> [Int] -> [Int] -> IsoMap -> Int -> Int -> Either IsoFailData IsoMap
+    go rest !gr1Selected !gr2Selected !oldMapping !oldK !oldX | gr1Subgr == rename mapping (subgraph gr2Selected gr2) = descend rest
       where
         gr1Subgr = subgraph gr1Selected gr1
         descend [] | gr1Subgr == gr1 && order gr2 == order gr1Subgr = Right mapping
-                   | otherwise = Left [mapping]
-        descend (x:xs) = sumEither $ map selectX (nodes gr2)
-          where selectX k = go xs (x:gr1Selected) (k:gr2Selected) (IntMap.insert k x mapping)
-    go _ _ _ m = Left [m]
+                   | otherwise = Left (oldMapping, Just (oldK, oldX))
+        descend (x:xs) = selectX `mapsEither` nodes gr2
+          where selectX k = go xs (oldX:gr1Selected) (oldK:gr2Selected) mapping k x
+        !mapping = IntMap.insert oldK oldX oldMapping
+    go _ _ _ m k x = Left (m, Just (k, x))
 
     rename mapping gr = mkGraph ns es
       where
-        ns = map (first newName) (labNodes gr)
+        ns = first newName <$> labNodes gr
         es = map (\(a, b, c) -> (newName a, newName b, c)) (labEdges gr)
         newName node = fromMaybe (error $ "Invariant broken: missing mapping for node " ++ show node) $ IntMap.lookup node mapping
+matchGraph gr1 gr2 | order gr1 == 0 && order gr2 == 0 = Right mempty
+matchGraph _ _ = emptyEither
 
 matchAndReport :: (Eq a, Ord b, Show a, Show b) => Gr a b -> Gr a b -> Expectation
 matchAndReport gr1 gr2 =
     case matchGraph gr1 gr2 of
         Right match -> return ()
-        Left [] -> putStrLn "I could not match any part of the two graphs"
-        Left matches -> 
-            let largest = maximumBy (compare `on` IntMap.size) matches
-                selectedGr1Nodes = IntMap.elems largest
+        Left (largest, keys) -> 
+            let selectedGr1Nodes = IntMap.elems largest
                 selectedGr2Nodes = IntMap.keys largest
+                unselectedGr1Nodes = filter (not . flip IntSet.member (IntSet.fromList selectedGr1Nodes) . fst) (labNodes gr1)
+                unselectedGr2Nodes = filter (not . flip IntSet.member (IntMap.keysSet largest) . fst) (labNodes gr2)
             in do
-                putStrLn "The largest match was between\n"
-                putStrLn $ prettify (subgraph selectedGr1Nodes gr1)
-                putStrLn $ "\nand\n"
-                putStrLn $ prettify (subgraph selectedGr2Nodes gr2)
-                putStrLn $ "\nI could not match the nodes\n"
-                putStrLn $ show $ filter (not . flip IntSet.member (IntSet.fromList selectedGr1Nodes) . fst) (labNodes gr1)
-                putStrLn $ "\nwith\n"
-                putStrLn $ show $ filter (not . flip IntSet.member (IntMap.keysSet largest) . fst) (labNodes gr2)
+                expectationFailure $ unlines
+                    [ "Graphs weren't isomorphic."
+                    , "The largest match was between"
+                    , ""
+                    , prettify (subgraph selectedGr1Nodes gr1)
+                    , ""
+                    , "and"
+                    , ""
+                    , prettify (subgraph selectedGr2Nodes gr2)
+                    , ""
+                    , "I could not match the nodes"
+                    , ""
+                    , show $ unselectedGr1Nodes
+                    , ""
+                    , "with"
+                    , ""
+                    , show $ unselectedGr2Nodes
+                    , case keys of
+                        Nothing -> ""
+                        Just (k, x) -> unlines
+                            [ ""
+                            , "I failed when matching"
+                            , show $ filter ((== x) . fst) unselectedGr1Nodes
+                            , show $ filter ((== k) . fst) unselectedGr1Nodes
+                            ]
+                    ]
