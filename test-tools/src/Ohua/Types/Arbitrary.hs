15e65f91f4ffa26f228e6ec7dea3cffa3a22456f
Justus Adam <dev@justus.science>
2018-10-22 16:52:33 +0200

    Working on fixing the parser and pretty printer for dflang
    Disabling the random tests, because they do not terminate :(

--- test-tools/src/Ohua/Types/Arbitrary.hs
+++ test-tools/src/Ohua/Types/Arbitrary.hs
@@ -47,16 +47,21 @@ instance Arbitrary a => Arbitrary (Source a) where
 instance Arbitrary a => Arbitrary (AbstractOutGraph a) where
     arbitrary = liftM3 OutGraph arbitrary arbitrary arbitrary
 
+isValidForBinding = not . flip HS.member reservedWords
 
 instance Arbitrary Binding where
     arbitrary = do
         a <- oneof $ map pure idstartchars
         as <- listOf $ oneof $ map pure idchars
-        pure (unsafeMake $ toText $ a : as)
-          `suchThat` (not . flip HS.member reservedWords)
+        pure (unsafeMake $ toText $ a : as) `suchThat` isValidForBinding
       where
         idstartchars = ['a' .. 'z'] <> ['A' .. 'Z']
         idchars = idstartchars <> ['0' .. '9'] <> ['_']
+    shrink (unwrap -> b) =
+        [ b'
+        | b' <- unsafeMake . toText <$> shrink (toString b)
+        , isValidForBinding b'
+        ]
 
 instance Arbitrary QualifiedBinding where
     arbitrary = QualifiedBinding <$> arbitrary <*> arbitrary
@@ -78,6 +83,8 @@ instance Arbitrary Assignment where
             [ Direct <$> arbitrary
             , Destructure <$> (arbitrary `suchThat` ((/= 1) . length))
             ]
+    shrink (Destructure ds) = map Destructure $ shrink ds
+    shrink _ = []
 instance Arbitrary a => Arbitrary (Expr a) where
     arbitrary = sized expr
       where
@@ -91,21 +98,24 @@ instance Arbitrary a => Arbitrary (Expr a) where
             ]
           where
             nestExpr = expr $ n `div` 2
-    shrink (Let _ a b)  = [a,b]
-    shrink (Apply a b)  = [a,b]
-    shrink (Lambda _ a) = [a]
+    shrink (Let a b c)  = b:c: [Let a b' c' | (b', c') <- shrink (b, c)]
+    shrink (Apply a b)  = a:b: [Apply a' b' | (a', b') <- shrink (a, b)]
+    shrink (Lambda a b) = b: map (Lambda a) (shrink b)
     shrink _            = []
 
 instance Arbitrary a => Arbitrary (Namespace a) where
     arbitrary = do
-      n <- arbitrary
-      algoImports0 <- arbitrary
-      sfImports0 <- arbitrary
-      decls0 <- arbitrary
-      pure $ (emptyNamespace n :: Namespace ())
-        & algoImports .~ algoImports0
-        & sfImports .~ sfImports0
-        & decls .~ HM.fromList decls0
+        n <- arbitrary
+        algoImports0 <- arbitrary
+        sfImports0 <- arbitrary
+        decls0 <- arbitrary
+        pure $
+            (emptyNamespace n :: Namespace ()) & algoImports .~ algoImports0 &
+            sfImports .~ sfImports0 &
+            decls .~ HM.fromList decls0
+    shrink ns = do
+        (ai, si, de) <- shrink (ns ^. algoImports, ns ^. sfImports, HM.toList $ ns ^. decls)
+        pure $ ns & algoImports .~ ai & sfImports .~ si & decls .~ HM.fromList de
 
 
 instance Arbitrary DFExpr where
@@ -116,9 +126,17 @@ instance Arbitrary LetExpr where
     arbitrary =
         LetExpr <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*>
         arbitrary
+    shrink (LetExpr id r fr ca ctx) =
+        [ LetExpr id' r' fr' ca' ctx'
+        | (id', r', fr', ca', ctx') <- shrink (id, r, fr, ca, ctx)
+        ]
 
 instance Arbitrary DFVar where
     arbitrary = oneof [DFEnvVar <$> arbitrary, DFVar <$> arbitrary]
+    shrink (DFEnvVar e) = map DFEnvVar $ shrink e
+    shrink (DFVar b) = map DFVar $ shrink b
 
 instance Arbitrary DFFnRef where
     arbitrary = oneof [DFFunction <$> arbitrary, EmbedSf <$> arbitrary]
+    shrink (DFFunction f) = map DFFunction $ shrink f
+    shrink (EmbedSf f) = map EmbedSf $ shrink f
