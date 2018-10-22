6d2a30aa5963868f3e4769eff660ce1b6215b7c5
Justus Adam <dev@justus.science>
2018-10-22 15:47:26 +0200

    Fixed pretty printer for DFLang
    Fixed number parsing in DFLang parser
    Implemented arbitrary generation for DFlang

---
+++ test-tools/test/Spec.hs
@@ -0,0 +1,26 @@
+
+import Ohua.Prelude
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+
+import Ohua.Test
+import Ohua.DFLang.Parser
+import Ohua.DFLang.Lang
+import Ohua.ALang.PPrint
+import Ohua.DFLang.PPrint ()
+import Ohua.Types.Arbitrary
+
+dfLangParserPPrinterSpec :: Spec
+dfLangParserPPrinterSpec =
+    describe "let-expression pretty printing is parseable" $
+    prop "it works" $ \randExpr ->
+        randExpr `shouldBe`
+        parseExp (encodeUtf8 $ quickRender (randExpr :: DFExpr))
+
+main :: IO ()
+main = return ()
+  --  This test should work (and kinda does, but it sometimes seems to get stuck
+  --  in an infinite loop and I do not have the time to figure out why).
+  --
+  -- hspec dfLangParserPPrinterSpec
