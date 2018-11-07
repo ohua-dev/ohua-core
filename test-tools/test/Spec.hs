fdc6d0344f3118073b5782b9bdba273e0d8eb000
Justus Adam <dev@justus.science>
2018-11-07 15:56:07 +0100

    Removed intermediate printing from the DFLang parser tests

--- test-tools/test/Spec.hs
+++ test-tools/test/Spec.hs
@@ -11,21 +11,15 @@ import Ohua.ALang.PPrint
 import Ohua.DFLang.PPrint ()
 import Ohua.Types.Arbitrary
 
-import System.IO.Unsafe
-
 dfLangParserPPrinterSpec :: Spec
 dfLangParserPPrinterSpec =
     describe "let-expression pretty printing is parseable" $
     prop "it works" $ \randExpr ->
         randExpr `shouldBe`
-        let rendered = quickRender (randExpr :: DFExpr)
-         in unsafePerformIO $ (putStrLn rendered) >>
-            return (parseExp (encodeUtf8 $ rendered))
+        parseExp (encodeUtf8 $ quickRender (randExpr :: DFExpr))
 
 main :: IO ()
-main =
-  return ()
+main
   --  This test should work (and kinda does, but it sometimes seems to get stuck
   --  in an infinite loop and I do not have the time to figure out why).
-  --
-  -- hspec dfLangParserPPrinterSpec
+ = unless True $ hspec dfLangParserPPrinterSpec
