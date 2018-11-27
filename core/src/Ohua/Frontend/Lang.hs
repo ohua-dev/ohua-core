d03d45df68df64addf4ebeabb625cd22a148bc7c
Justus Adam <dev@justus.science>
2018-11-27 11:42:10 +0100

    [#23] Split up the frontend language passes

--- core/src/Ohua/Frontend/Lang.hs
+++ core/src/Ohua/Frontend/Lang.hs
@@ -84,65 +84,85 @@ instance Biplate Expr Pat where
 instance Hashable Expr
 instance NFData Expr
 
+-- | Not sure this traversal is necessary, but it makes every smap argument into
+-- a lambda
+--
+-- I am leaving it here in case we need it later.
+ensureLambdaInSmap :: (Monad m, MonadGenBnd m) => Expr -> m Expr
+ensureLambdaInSmap =
+    rewriteM $ \case
+        MapE (LamE _ _) _ -> pure Nothing
+        MapE other coll -> do
+            bnd <- generateBinding
+            pure $ Just $ MapE (LamE [VarP bnd] $ AppE other [VarE bnd]) coll
+        _ -> pure Nothing
 
-toAlang :: MonadOhua m => Expr -> m AL.Expr
-toAlang = mkSingleLamApply >>> removeDestructuring >=> trans
+-- | Ensures every lambda takes at most one argument.
+mkLamSingleArgument :: Expr -> Expr
+mkLamSingleArgument =
+    rewrite $ \case
+        LamE (x1:x2:xs) b -> Just $ LamE [x1] $ LamE (x2 : xs) b
+        _ -> Nothing
+
+removeDestructuring :: (MonadGenBnd m, Monad m) => Expr -> m Expr
+removeDestructuring =
+    rewriteM $ \case
+        LetE (TupP pats) e1 e2 -> do
+            valBnd <- generateBinding
+            pure $ Just $ LetE (VarP valBnd) e1 $ unstructure valBnd pats e2
+        LamE [TupP pats] e -> do
+            valBnd <- generateBinding
+            pure $ Just $ LamE [VarP valBnd] $ unstructure valBnd pats e
+        _ -> pure Nothing
+
+giveEmptyLambdaUnitArgument :: Expr -> Expr
+giveEmptyLambdaUnitArgument = rewrite $ \case
+    LamE [] e -> Just $ LamE [UnitP] e
+    _ -> Nothing
+
+nthFun :: Expr
+nthFun = LitE $ FunRefLit $ FunRef "ohua.lang/nth" Nothing
+
+unstructure :: Binding -> [Pat] -> Expr -> Expr
+unstructure valBnd =
+    foldl (.) id .
+    map (\(idx, pat) ->
+             LetE pat $ AppE nthFun [LitE (NumericLit idx), VarE valBnd]) .
+    zip [0 ..]
+
+trans :: Expr -> AL.Expr
+trans =
+    cata $ \case
+        VarEF b -> Var b
+        LitEF l -> Lit l
+        LetEF p e1 e2 -> Let (patToBnd p) e1 e2
+        AppEF e1 e2
+            | null e2 -> e1 `Apply` Lit UnitLit
+            | otherwise -> foldl Apply e1 e2
+        LamEF p e ->
+            case p of
+                [] -> e
+                [p] -> Lambda (patToBnd p) e
+                _ ->
+                    error $
+                    "Invariant broken: Found multi apply or destucture lambda: " <>
+                    show p
+        IfEF cont then_ else_ ->
+            ifBuiltin `Apply` cont `Apply` Lambda "_" then_ `Apply`
+            Lambda "_" else_
+        MapEF function coll -> smapBuiltin `Apply` function `Apply` coll
+        BindEF e1 e2 -> error "State binding not yet implemented in ALang"
+        StmtEF e1 cont -> Let "_" e1 cont
+        SeqEF source target -> error "Seq not yet implemented"
+        TupEF parts -> foldl Apply (Sf Refs.mkTuple Nothing) parts
   where
-    mkSingleLamApply =
-        rewrite $ \case
-            LamE (x1:x2:xs) b -> Just $ LamE [x1] $ LamE (x2 : xs) b
-            other -> Nothing
-    removeDestructuring =
-        rewriteM $ \case
-            LetE (TupP bnds) e1 e2 -> do
-                valBnd <- generateBinding
-                pure $ Just $ LetE (VarP valBnd) e1 $ unstructure valBnd bnds e2
-            LamE [TupP bnds] e -> do
-                valBnd <- generateBinding
-                pure $ Just $ LamE [VarP valBnd] $ unstructure valBnd bnds e
-            _ -> pure Nothing
-    nthFun = LitE $ FunRefLit $ FunRef "ohua.lang/nth" Nothing
-    unstructure valBnd bnds =
-        foldl (.) id $
-        map
-            (\(idx, bnd) ->
-                 LetE bnd $ AppE nthFun [LitE (NumericLit idx), VarE valBnd])
-            (zip [0 ..] bnds)
-    sfE name = Lit $ FunRefLit $ FunRef name Nothing
-    trans =
-        cata $
-        sequence >=> \case
-            VarEF b -> pure $ Var b
-            LitEF l -> pure $ Lit l
-            LetEF p e1 e2 ->
-                case p of
-                    VarP b -> pure $ Let b e1 e2
-                    _ ->
-                        throwErrorDebugS $
-                        "Invariant broken: Found destructure pattern: " <> show p
-            AppEF e1 e2
-                | null e2 -> pure $ e1 `Apply` Lit UnitLit
-                | otherwise -> pure $ foldl Apply e1 e2
-            LamEF p e ->
-                case p of
-                    [] -> pure $ Lambda "_" e
-                    [VarP b] -> pure $ Lambda b e
-                    _ ->
-                        throwErrorDebugS $
-                        "Invariant broken: Found multi apply or destucture lambda: " <>
-                        show p
-            IfEF cont then_ else_ ->
-                pure $
-                ifBuiltin `Apply` cont `Apply` Lambda "_" then_ `Apply`
-                Lambda "_" else_
-            MapEF function coll -> do
-                lamBnd <- generateBinding
-                pure $
-                    smapBuiltin `Apply`
-                    Lambda lamBnd (function `Apply` Var lamBnd) `Apply`
-                    coll
-            BindEF e1 e2 ->
-                throwError "State binding not yet implemented in ALang"
-            StmtEF e1 cont -> pure $ Let "_" e1 cont
-            SeqEF source target -> throwError "Seq not yet implemented"
-            TupEF parts -> pure $ foldl Apply (sfE Refs.mkTuple) parts
+    patToBnd =
+        \case
+            VarP v -> v
+            UnitP -> "_"
+            p -> error $ "Invariant broken, invalid pattern: " <> show p
+
+toAlang :: (Monad m, MonadGenBnd m) => Expr -> m AL.Expr
+toAlang =
+    giveEmptyLambdaUnitArgument >>>
+    mkLamSingleArgument >>> removeDestructuring >=> pure . trans
