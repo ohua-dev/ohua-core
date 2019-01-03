e0536ebd80f7c60f918a1fb756d0993b804ca41c
Sebastian Ertel <sebastian.ertel@gmail.com>
2019-01-03 21:53:07 +0100

    fix

--- core/src/Ohua/DFLang/Passes/If.hs
+++ core/src/Ohua/DFLang/Passes/If.hs
@@ -14,7 +14,7 @@ optimizeIf :: DFExpr -> DFExpr
 optimizeIf (DFExpr letExprs returnVar) =
     let ifFuns = toList $ findAllExprs Refs.ifFun letExprs
         (newIfFuns, orphanedNths) = unzip $ toList $ map updateIfFuns ifFuns
-        orphanedNths' = catMaybes $ join orphanedNths
+        orphanedNths' = join $ catMaybes $ join orphanedNths
         letExprs' =
             removeAllExprs (DS.fromList $ ifFuns ++ orphanedNths') letExprs
      in flip DFExpr returnVar $ letExprs' >< DS.fromList newIfFuns
@@ -23,10 +23,10 @@ optimizeIf (DFExpr letExprs returnVar) =
         let (newOuts, nths) =
                 Ohua.Prelude.unzip $
                 flip map outs $ \out ->
-                    let (nth:[]) = findUsages out letExprs
-                        nthOuts = output nth
+                    let (nth1:nth2:[]) = findUsages out letExprs
+                        nthOuts = output nth1 ++ output nth2
                      in if (length nthOuts) == 1
-                            then (head $ NE.fromList nthOuts, Just nth)
+                            then (head $ NE.fromList nthOuts, Just [nth1, nth2])
                             else error "Invariant broken"
          in ( ifFun {output = newOuts, functionRef = DFFnRef OperatorNode qb}
             , nths)
