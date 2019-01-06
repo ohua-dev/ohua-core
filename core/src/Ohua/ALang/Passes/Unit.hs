b97176cfaf6559d6156241e80df495e58d0a8700
Sebastian Ertel <sebastian.ertel@gmail.com>
2019-01-06 12:59:11 +0100

    fixing the hack

--- core/src/Ohua/ALang/Passes/Unit.hs
+++ core/src/Ohua/ALang/Passes/Unit.hs
@@ -7,13 +7,14 @@ import Ohua.ALang.Lang
 mkUnitFunctionsExplicit :: Expression -> Expression
 mkUnitFunctionsExplicit e =
     flip transform e $ \case
-        Let v (Apply f@(Lit (FunRefLit _)) u@(Lit UnitLit)) ie ->
+        Let v (Apply (Lit (FunRefLit (FunRef f _))) u@(Lit UnitLit)) ie ->
             Let
                 v
                 -- FIXME the -1 is a hack because serialization otherwise prints null.
                 --       it feels to me that the id should be part of the outermost Lit
                 --       instead of the FunRef
-                ((Lit $ FunRefLit $ FunRef "lang/unitFn" $ Just (-1)) `Apply` f `Apply`
+                ((Lit $ FunRefLit $ FunRef "lang/unitFn" Nothing) `Apply`
+                 (Lit $ FunRefLit $ FunRef f $ Just (-1)) `Apply`
                  u)
                 ie
         other -> other
