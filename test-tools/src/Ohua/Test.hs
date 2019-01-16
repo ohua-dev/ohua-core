f7adf10a8a95532a91d39a30d08f8f0a2a9a4771
Justus Adam <dev@justus.science>
2019-01-16 11:53:10 +0100

    [#26] Added a test helper for pretty printing test failures

--- test-tools/src/Ohua/Test.hs
+++ test-tools/src/Ohua/Test.hs
@@ -1,18 +1,17 @@
-module Ohua.Test where
+module Ohua.Test (embedALang, embedDFLang, showWithPretty) where
 
 import Ohua.Prelude hiding (lift)
 
 import Language.Haskell.TH.Quote
-import Language.Haskell.TH.Syntax (lift, Exp, Q)
+import Language.Haskell.TH.Syntax (lift)
 import qualified Data.ByteString.Lazy.Char8 as LB
-import qualified Data.HashSet as HS
 
-import Ohua.ALang.Lang
+import Ohua.ALang.PPrint (Pretty, pretty)
 import Ohua.Frontend.Lang
-import Ohua.DFLang.Lang
 import qualified Ohua.DFLang.Parser as ParseDFLang
 import qualified Ohua.Compat.ML.Parser as ParseALang
 
+import qualified Prelude (show)
 
 embedALang :: QuasiQuoter
 embedALang =
@@ -42,3 +41,18 @@ embedDFLang =
         }
   where
     notDefined = const $ fail "DFLang can only be embedded as an expression"
+
+
+newtype PrettyAsShow a = PrettyAsShow { unPrettyAsShow :: a } deriving (Eq)
+
+instance Pretty a => Show (PrettyAsShow a) where
+    show = show . pretty . unPrettyAsShow
+
+-- | Helper function for working with `hspec` tests.
+--
+-- This wraps the data (@a@) into a 'PrettyAsShow' newtype which implements 'Eq'
+-- simply as the 'Eq' defined on the @a@ but overrides 'Show' by calling
+-- 'pretty'. This is useful in tests for ALang of DFLang etc where the default
+-- 'Show' implementation is often hard to read.
+showWithPretty :: a -> PrettyAsShow a
+showWithPretty = PrettyAsShow
