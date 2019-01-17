0a72ed7ca0b32e00222af56c71da0289373bbe56
Justus Adam <dev@justus.science>
2019-01-17 14:32:08 +0100

    [#27] Added some basic tests for dflang parsing
    Closes #27

--- test-tools/test/Spec.hs
+++ test-tools/test/Spec.hs
@@ -1,6 +1,8 @@
 
 import Ohua.Prelude
 
+import Control.Exception (evaluate)
+
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
@@ -18,8 +20,61 @@ dfLangParserPPrinterSpec =
         randExpr `shouldBe`
         parseExp (encodeUtf8 $ quickRender (randExpr :: DFExpr))
 
+dflangParseSpec :: Spec
+dflangParseSpec =
+    describe "dflang parsing" $ do
+        let op = DFFnRef OperatorNode
+            sf = DFFnRef FunctionNode
+            num = DFEnvVar . NumericLit
+            env = DFEnvVar . EnvRefLit
+        it "parses a simple let expression" $
+            parseExp "let (a) = b/c<0>() in d" `shouldBe`
+            DFExpr [LetExpr 0 ["a"] (sf "b/c") Nothing []] "d"
+        it "parses a let expression with variable arguments" $ do
+            parseExp "let (a) = some.ns/func<2>(a) in var" `shouldBe`
+                DFExpr [LetExpr 2 ["a"] (sf "some.ns/func") Nothing ["a"]] "var"
+            parseExp "let (a) = some.other.ns/func2<2>(a, b_d, ugI) in var" `shouldBe`
+                DFExpr
+                    [ LetExpr
+                          2
+                          ["a"]
+                          (sf "some.other.ns/func2")
+                          Nothing
+                          ["a", "b_d", "ugI"]
+                    ]
+                    "var"
+        it "parses let expressions with multiple returns" $
+            parseExp "let (a, f_po0, _8) = b/c<0>() in d" `shouldBe`
+            DFExpr [LetExpr 0 ["a", "f_po0", "_8"] (sf "b/c") Nothing []] "d"
+        it "parses let expressions with numeric literal arguments" $
+            parseExp "let (a) = some.ns/func<2>(0, -80, 1000) in var" `shouldBe`
+                DFExpr
+                    [ LetExpr
+                          2
+                          ["a"]
+                          (sf "some.ns/func")
+                          Nothing
+                          [num 0, num (-80), num 1000]
+                    ]
+                    "var"
+        it "parses let expressions with env literal arguments" $
+            parseExp "let (a) = some.ns/func<2>($0, $80, $1000) in var" `shouldBe`
+                DFExpr
+                    [ LetExpr
+                          2
+                          ["a"]
+                          (sf "some.ns/func")
+                          Nothing
+                          [env 0, env 80, env 1000]
+                    ]
+                    "var"
+        it "fails to parse negative env literals" $
+            evaluate (parseExp "let (a) = a/b<5>($-9) in b") `shouldThrow` anyErrorCall
+
 main :: IO ()
 main
   --  This test should work (and kinda does, but it sometimes seems to get stuck
   --  in an infinite loop and I do not have the time to figure out why).
- = unless True $ hspec dfLangParserPPrinterSpec
+ = hspec $ do
+    dflangParseSpec
+    unless True dfLangParserPPrinterSpec
