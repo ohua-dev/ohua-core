fd966e821294facdff73901912e1185cdcce09ca
Justus Adam <dev@justus.science>
2019-09-02 14:36:21 +0200

    Rewrote TailRec dflang transformation

--- core/src/Ohua/Feature/TailRec/Passes/DFLang.hs
+++ core/src/Ohua/Feature/TailRec/Passes/DFLang.hs
@@ -16,34 +16,32 @@ recurLowering (DFExpr letExprs returnVar)
   -- 1. Find the recurFun with two outputs
  =
     flip DFExpr returnVar $
-    DS.fromList $
-    filter ((/= ALangPass.recurEndMarker) . nodeRef . functionRef) $
-    transform transformator (toList letExprs)
+    DS.fromList $ transform transformator (toList letExprs)
   where
     transformator [] = []
-    transformator l@(recurStart:rest) =
-        if nodeRef (functionRef recurStart) /= ALangPass.recurStartMarker
-            then l
-            else assert (length (output recurStart) == 2) $
-                 let findEnd l@(LetExpr {functionRef = f})
-                         | nodeRef f == ALangPass.recurEndMarker = l
-                              -- all paths lead to the final recurFun because this is a connected component where
-                            -- the recurFun at the very end has the only outgoing arc.
-                     findEnd e = findEnd $ allSuccessors e
-                     allSuccessors = head . (flip findUsages rest <=< output)
-                     endFunction = findEnd $ allSuccessors recurStart
-                     fixRef:cond:recurArgs = callArguments endFunction
-                  in recurStart
-                         { output = output recurStart <> output endFunction
-                         , functionRef = Refs.recurFun
+    transformator l@(recurStart:rest)
+        | nodeRef (functionRef recurStart) /= ALangPass.recurStartMarker = l
+        | otherwise =
+            assert (length (output recurStart) == 2) $
+            recurStart
+                { output = output recurStart <> output endFunction
+                , functionRef = Refs.recurFun
                               -- FIXME we don't need the var lists when we use the assertion
                               -- that these two lists have the same size! and this is always
                               -- true because these are the arguments to a call to the same
                               -- function, i.e, the recursion!
-                         , callArguments =
-                               fixRef :
-                               cond : callArguments recurStart <> recurArgs
-                         } :
-                     rest
+                , callArguments =
+                      fixRef : cond : callArguments recurStart <> recurArgs
+                } :
+            delete endFunction rest
+      where
+        findEnd l1@(LetExpr {functionRef = f})
+            | nodeRef f == ALangPass.recurEndMarker = l1
+              -- all paths lead to the final recurFun because this is a connected component where
+              -- the recurFun at the very end has the only outgoing arc.
+        findEnd e = findEnd $ allSuccessors e
+        allSuccessors = head . (flip findUsages rest <=< output)
+        endFunction = findEnd $ allSuccessors recurStart
+        fixRef:cond:recurArgs = callArguments endFunction
     head (x:_) = x
     head _ = error "head"
